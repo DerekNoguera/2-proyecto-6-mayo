0f5084fd05faf87e350e2eadfd132fa5ac27dfab
DerekNoguera <dereknoguera9@gmail.com>
2024-05-10 13:38:34 -0600

    la parte funcional de el login recibiendo los datos de localstorage

--- js/login.js
+++ js/login.js
@@ -1,5 +1,21 @@
-let userLogin = document.getElementById("user")
-let passwordLogin = document.getElementById("password")
+let login1 = document.getElementById("login1")
+login1.addEventListener("click", (e) => {
+    e.preventDefault()
+    console.log(login1);
+    let emailLogin = document.getElementById("user")
+    let passwordLogin = document.getElementById("password")
+
+    let Users = JSON.parse(localStorage.getItem('users')) || [] //OBTIENE los elementos de localStorage y su contraseña es 'users'
+    let validarCampos = Users.find(user => user.email === emailLogin.value && user.password === passwordLogin.value)//en la key users va a encontrar
+    // o va a validar que emaillogin sea estrictamente igual a el email y passwordLogin sea estricatamente igual a la contraseña que tiene guardada la key'users'
+    
+    if (!validarCampos) {// si validarCampos no es verdadero, osea es falso so va a enviar una alerta diciendo que el correo o contraseña es incorrecta
+        alert("Correo y/o contraseña incorrecta")
+    } else {
+        alert("Login Exitoso")
+        window.location.href = "index.html" //si validarCampos es verdadero entonces se va a enviar a la otra pagina
+    }
+})
 
 // R E G I S T E R
 
@@ -13,7 +29,7 @@ registros.addEventListener("click", (e) => {
 
     let users = JSON.parse(localStorage.getItem('users')) || [] // Users va a contener mis usuarios, contraseñas e email, JSON.parse(localStorage.getItem('users') dice //que obtenga los elementos de localStorage('user') y se especifica y los [] si el local tiene contenido lo va a guardar en users y si no, va a ser un array vacio
     let usuariosRegistrados = users.find(user => user.email === email)//usuariosRegistrados va a ser igual a user.find es encontrar usuario y va a buscar si user => //user.email es igual a email
-   // solamente el registro va a funcionar si el email no es estrictamente igual email, si no es el mismo email entonces se puede registrar
+    // solamente el registro va a funcionar si el email no es estrictamente igual email, si no es el mismo email entonces se puede registrar
     if (usuariosRegistrados) {
         return alert("El usuario ya esta registrado") //si usuarios registrado es verdadero entonces quiero que me retorne una alerta
     }
