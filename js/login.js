7b203a98c24d99e44667d18cb692735a1d3ed312
DerekNoguera <dereknoguera9@gmail.com>
2024-05-10 10:39:40 -0600

    login y register estructura, con el css y su js

---
+++ js/login.js
@@ -0,0 +1,10 @@
+let user = document.getElementById("user")
+let password = document.getElementById("password")
+console.log(user,password);
+
+// R E G I S T E R
+
+let email = document.getElementById("email")
+let userRegister = document.getElementById("user-register")
+let passwordRegister = document.getElementById("password-register")
+console.log(email, userRegister, passwordRegister);
